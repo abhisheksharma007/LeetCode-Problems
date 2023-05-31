8bbd9a8e7efdfc23e70ec4e56916e0a6643ecf9b
abhisheksharma007 <67024781+abhisheksharma007@users.noreply.github.com>
2023-05-31 17:09:50 +0000

    test

--- test.py
+++ test.py
@@ -1,3 +1,31 @@
-import time
+from math import ceil
 
-print("test")
+piles = [30,11,23,4,20]
+h = 6
+
+def min_bananas_per_hr(piles, h):
+    
+    if len(piles) == h:
+        return max(piles)
+    
+    l = 0
+    r = max(piles)
+    res = max(piles)
+    
+    while l < r:
+        x = 0
+        mid = (l+r)//2
+        
+        for i in piles:
+            x += ceil(i/mid)
+
+        if x <= h and mid < res:
+            res = mid
+        elif x > h:
+            l = mid+1
+        else:
+            r = mid-1
+        
+    return res
+
+print(min_bananas_per_hr(piles, h))
