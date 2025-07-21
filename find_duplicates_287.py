8715c3515fdd1eae5dec70efb696ddd36e500753
abhisheksharma007 <abhishek.sharma@monetate.com>
2025-07-22 01:30:37 +0530

    floyds tortoise and hair algo 287

---
+++ find_duplicates_287.py
@@ -0,0 +1,24 @@
+# contraint O(1)
+
+nums = [1,3,4,2,2]
+# nums = [3,1,3,4,2]
+# nums = [3,3,3,3,3]
+
+
+def find_duplicate(nums):
+    s, f = 0, 0
+    while True:
+        s = nums[s]
+        f = nums[nums[f]]
+        if s == f:
+            break
+
+    s2 = 0
+    while True:
+        s = nums[s]
+        s2 = nums[s2]
+        if s == s2:
+            return s
+
+print(find_duplicate(nums))
+
